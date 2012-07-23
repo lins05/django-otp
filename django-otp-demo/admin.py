c0cffa5a1d57c3d3251bdc455ad017035a11b232
Peter Sagerson <psagers.hg@ignorare.net>
2012-07-22 22:17:42 -0700

    OATH implementation and YubiKey tests.

--- django-otp-demo/admin.py
+++ django-otp-demo/admin.py
@@ -5,6 +5,8 @@ from django_otp.plugins.otp_static.admin import StaticDevice, StaticDeviceAdmin
 from django_otp.plugins.otp_email.admin import EmailDevice, EmailDeviceAdmin
 from otp_yubikey.admin import YubikeyDevice, YubikeyDeviceAdmin
 from otp_yubikey.admin import RemoteYubikeyDevice, RemoteYubikeyDeviceAdmin
+from otp_oath.admin import HOTPDevice, HOTPDeviceAdmin
+from otp_oath.admin import TOTPDevice, TOTPDeviceAdmin
 
 
 site = OTPAdminSite()
@@ -15,3 +17,5 @@ site.register(StaticDevice, StaticDeviceAdmin)
 site.register(EmailDevice, EmailDeviceAdmin)
 site.register(YubikeyDevice, YubikeyDeviceAdmin)
 site.register(RemoteYubikeyDevice, RemoteYubikeyDeviceAdmin)
+site.register(HOTPDevice, HOTPDeviceAdmin)
+site.register(TOTPDevice, TOTPDeviceAdmin)
